2b58ed7aec951792ef8be4531285b0395c596d53
Sven Fuchs <svenfuchs@artweb-design.de>
2009-09-02 15:13:30 +0200

    extract a reusable Rack::Cache::Tools:Key that takes both a Rack::Request and/or uri

--- lib/rack/cache/tools.rb
+++ lib/rack/cache/tools.rb
@@ -1,3 +1,4 @@
 module Rack::Cache::Tools
+  autoload :Key,     'rack/cache/tools/key'
   autoload :Options, 'rack/cache/tools/options'
 end
---
+++ lib/rack/cache/tools/key.rb
@@ -0,0 +1,96 @@
+require 'rack/utils'
+
+module Rack::Cache::Tools
+  class Key
+    # Implement .call, since it seems like the "Rack-y" thing to do. Plus, it
+    # opens the door for cache key generators to just be blocks.
+    def self.call(*args)
+      new(*args).generate
+    end
+
+    def initialize(request, uri = nil)
+      self.request = request
+      self.uri = uri
+      expand_relative_uri! if uri
+    end
+
+    attr_reader :request, :uri
+
+    def source
+      @source ||= uri || request
+    end
+    
+    %w(scheme host port path query script_name).each do |method|
+      class_eval "def #{method}; source.#{method}; end"
+    end
+
+    def generate
+      parts = []
+      parts << scheme << "://"
+      parts << host
+      parts << ":" << port.to_s if irregular_port?
+      parts << script_name
+      parts << path
+      parts << "?" << normalize_query(query) unless query.nil? || query.empty?
+      parts.join
+    end
+
+    protected
+
+      def request=(request)
+        @request = Request.new(request)
+      end
+
+      def uri=(uri)
+        uri  = URI.parse(uri) unless uri.respond_to?(:scheme)
+        @uri = Uri.new(uri)
+      end
+
+      def expand_relative_uri!
+        uri.scheme ||= request.scheme
+        uri.host   ||= request.host
+        uri.port   ||= request.port
+      end
+
+      def irregular_port?
+        uri.scheme == "https" && uri.port != 443 ||
+        uri.scheme == "http" && uri.port != 80
+      end
+
+      def normalize_query(query)
+        query.
+          split(/[&;] */n).
+          map { |p| unescape(p).split('=', 2) }.
+          sort.
+          map { |k,v| "#{escape(k)}=#{escape(v)}" }.
+          join('&')
+      end
+
+      # simple adapters to straighten URI and Rack::Request APIs 
+      class Adapter
+        def initialize(target)
+          @target = target
+        end
+
+        def method_missing(*args, &block)
+          @target.send(*args, &block)
+        end
+      end
+    
+      class Uri < Adapter
+        def script_name
+          nil
+        end
+      end
+    
+      class Request < Adapter
+        def path
+          @target.path_info
+        end
+      
+        def query
+          @target.query_string
+        end
+      end
+  end
+end
