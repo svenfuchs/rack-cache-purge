5bf659cc2519428c215352979247869771f70105
Sven Fuchs <svenfuchs@artweb-design.de>
2010-09-26 23:09:21 +0200

    don't remove purge headers for now

--- lib/rack/cache/purge/base.rb
+++ lib/rack/cache/purge/base.rb
@@ -12,7 +12,12 @@ module Rack
         def call(env)
           purger = Purger.new(env)
           status, headers, body = app.call(env.merge(PURGER_HEADER => purger))
-          purger.purge(headers.delete(PURGE_HEADER)) if headers.key?(PURGE_HEADER)
+
+          # TODO we probably should remove the headers here but i can't figure out
+          # how to get the middlewares correctly pushed to the rails middleware stack
+          # purger.purge(headers.delete(PURGE_HEADER)) if headers.key?(PURGE_HEADER)
+
+          purger.purge(headers[PURGE_HEADER]) if headers.key?(PURGE_HEADER)
           [status, headers, body]
         end
       end
